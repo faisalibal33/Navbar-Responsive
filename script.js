904fa69da9a8d4c2aebdde7c9fc81091e514281e
faisalibal33 <faisaliqbal030397@gmail.com>
2022-02-25 13:47:51 +0700

    edit side bar toggle

--- script.js
+++ script.js
@@ -2,12 +2,36 @@ const body = document.querySelector("body")
 const nav = document.querySelector("nav")
 const modeToggle = document.querySelector(".dark-light")
 const searchToggle = document.querySelector(".searchToggle")
+const sidebarOpen = document.querySelector(".sidebarOpen")
+
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark-mode"){
+    body.classList.add("dark")
+}
 
 modeToggle.addEventListener("click", () => {
     modeToggle.classList.toggle("active")
     body.classList.toggle("dark")
+
+    if(!body.classList.contains("dark")){
+        localStorage.setItem("mode", "light-mode");
+    }else{
+        localStorage.setItem("mode", "dark-mode");
+    }
 })
 
 searchToggle.addEventListener("click", () => {
     searchToggle.classList.toggle("active")
+})
+
+sidebarOpen.addEventListener("click", () => {
+    nav.classList.add("active");
+})
+
+body.addEventListener("click", e =>{
+    let removeMenu = e.target;
+
+    if (!removeMenu.classList.contains("sidebarOpen") && !removeMenu.classList.contains("menu")){
+        nav.classList.remove("active");
+    }
 })
